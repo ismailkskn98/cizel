c2a55c1941e9419df66642530fa045b2f1a300ef
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-09-10 13:58:40 +0300

    aaaa

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -31,7 +31,7 @@ export default function Header() {
     ]
 
     return (
-        <header className='relative z-40 w-full fluid gridContainer py-2'>
+        <header className='relative z-50 w-full fluid gridContainer py-2'>
             <div className='fluid absolute z-0 inset-x-0 top-0 h-20 bg-gradient-to-b from-black/80 to-transparent'></div>
             <main className='w-full flex justify-between lg:grid lg:grid-cols-6 items-center'>
                 <Image src={"/images/cizel-logo-white.png"} alt='cizel logo' width={100} height={100} className='relative z-20 justify-self-start object-contain object-center w-fit h-20' />
