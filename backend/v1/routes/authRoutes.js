85b4a77c0198260de5ea052917accc0cb14dedd6
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-10-03 10:15:26 +0300

    aa

--- backend/v1/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { listUsers } = require("../controllers/authController");
-
-const router = express.Router();
-
-router.get("/", listUsers);
-
-module.exports = router;
